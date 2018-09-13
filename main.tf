7b3e06af6ef729f691bfbbadfeb341995f2da723
Weston Bassler <wbassler@mesosphere.com>
2018-09-13 19:10:47 -0400

    initial commit

---
+++ main.tf
@@ -0,0 +1,31 @@
+resource "aws_route53_record" "instance_public" {
+  count = "${var.num}"
+  zone_id = "${var.aws_zone_id}"
+  #name    = "${var.hostname_format}${count.index + 1}-${var.cluster_name}.${var.domain}"
+  name    = "${format(var.hostname_format, (count.index + 1), var.domain, var.cluster_name)}"
+  type    = "${var.type}"
+  ttl     = "${var.ttl}"
+  records = ["${var.public_records}"]
+}
+resource "aws_route53_record" "instance_internal" {
+  count = "${var.num}"
+  zone_id = "${var.aws_zone_id}"
+  name    = "int-${format(var.hostname_format, (count.index + 1), var.domain, var.cluster_name)}"
+  type    = "${var.type}" 
+  ttl     = "${var.ttl}"
+  records = ["${var.private_records}"]
+}
+
+/*
+resource "aws_route53_record" "external_masters" {
+  zone_id = "${var.aws_zone_id}"
+  name    = "${var.cluster_name}.${var.domain}"
+  type    = "A"
+
+  alias {
+    name                   = "${var.aws_lb_alias_name}"
+    zone_id                = "${var.aws_zone_id}"
+    evaluate_target_health = false
+  }
+}
+*/
---
+++ outputs.tf
@@ -0,0 +1,7 @@
+output "public_fqdns" {
+  value = "${join("\n", aws_route53_record.instance_public.*.name)}"
+}
+
+output "private_fqdns" {
+    value = "${join("\n", aws_route53_record.instance_internal.*.name)}"
+}
