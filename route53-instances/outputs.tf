d14e4a2148c6f3006a5995a7bb95ed6642ce8660
Weston Bassler <wbassler@mesosphere.com>
2018-10-18 15:32:17 -0400

    another update

--- route53-instances/outputs.tf
+++ route53-instances/outputs.tf
@@ -1,11 +1,11 @@
 output "public_fqdns" {
   description = "List of Public FQDNs"
   #value = "${join("\n", aws_route53_record.instance_public.*.name)}"
-  value = "${module.aws_route53_record.instance_public.*.name}"
+  value = "${join("\n", aws_route53_record.instance_public.*.name)}"
 }
 
 output "private_fqdns" {
   description = "List of Private FQDNs"
   #value = "${join("\n", aws_route53_record.instance_internal.*.name)}"
-  value = "${module.aws_route53_record.instance_private.*.name}"
+  value = "${join("\n", aws_route53_record.instance_internal.*.name)}"
 }
