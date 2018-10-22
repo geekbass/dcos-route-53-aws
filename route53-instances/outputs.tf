62c50cc4ca55572d5f61f29c1abfc5caf2548cc4
Weston Bassler <wbassler@mesosphere.com>
2018-10-22 12:58:17 -0400

    updated outputs so that we could get the private and public IPs from it

--- route53-instances/outputs.tf
+++ route53-instances/outputs.tf
@@ -1,13 +1,19 @@
 output "public_fqdns" {
   description = "List of Public FQDNs"
-
-  #value = "${join("\n", aws_route53_record.instance_public.*.name)}"
-  value = "${join("\n", aws_route53_record.instance_public.*.name)}"
+  value       = "${join("\n", aws_route53_record.instance_public.*.name)}"
 }
 
 output "private_fqdns" {
   description = "List of Private FQDNs"
+  value       = "${join("\n", aws_route53_record.instance_internal.*.name)}"
+}
 
-  #value = "${join("\n", aws_route53_record.instance_internal.*.name)}"
-  value = "${join("\n", aws_route53_record.instance_internal.*.name)}"
+output "private_ips" {
+  description = "List of Private IPs"
+  value       = "${join("\n  - ", flatten(list(aws_route53_record.instance_public.*.records)))}"
+}
+
+output "public_ips" {
+  description = "List of Public IPs"
+  value       = "${join("\n  - ", flatten(list(aws_route53_record.instance_internal.*.records)))}"
 }
